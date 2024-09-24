dfc2677f7f6c162d9857aa6edb4f179c2bf1942f
Felix Kirchhoff <f.kirchhoff@proton.me>
2024-09-24 14:40:00 +0200

    remove getColor function

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -5,30 +5,7 @@ local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
 -- This is where you actually apply your config choices
-local function getColorByKey(window, key)
-    local current_scheme = wezterm.color.get_builtin_schemes()[window:effective_config().color_scheme]
 
-    local key_to_color = {
-        black = current_scheme.ansi[1],
-        red = current_scheme.ansi[2],
-        green = current_scheme.ansi[3],
-        yellow = current_scheme.ansi[4],
-        blue = current_scheme.ansi[5],
-        magenta = current_scheme.ansi[6],
-        cyan = current_scheme.ansi[7],
-        white = current_scheme.ansi[8],
-        black_bright = current_scheme.brights[1],
-        red_bright = current_scheme.brights[2],
-        green_bright = current_scheme.brights[3],
-        yellow_bright = current_scheme.brights[4],
-        blue_bright = current_scheme.brights[5],
-        magenta_bright = current_scheme.brights[6],
-        cyan_bright = current_scheme.brights[7],
-        white_bright = current_scheme.brights[8],
-    }
-
-    return key_to_color[key]
-end
 -- For example, changing the color scheme:
 config.color_scheme = 'Apprentice (Gogh)'
 
