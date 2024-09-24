81b7569b0034b02895597b9e3ea2d17f1842de32
Felix Kirchhoff <f.kirchhoff@proton.me>
2024-09-24 14:39:31 +0200

    add retro tabs for wezterm

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -5,9 +5,155 @@ local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
 -- This is where you actually apply your config choices
+local function getColorByKey(window, key)
+    local current_scheme = wezterm.color.get_builtin_schemes()[window:effective_config().color_scheme]
 
+    local key_to_color = {
+        black = current_scheme.ansi[1],
+        red = current_scheme.ansi[2],
+        green = current_scheme.ansi[3],
+        yellow = current_scheme.ansi[4],
+        blue = current_scheme.ansi[5],
+        magenta = current_scheme.ansi[6],
+        cyan = current_scheme.ansi[7],
+        white = current_scheme.ansi[8],
+        black_bright = current_scheme.brights[1],
+        red_bright = current_scheme.brights[2],
+        green_bright = current_scheme.brights[3],
+        yellow_bright = current_scheme.brights[4],
+        blue_bright = current_scheme.brights[5],
+        magenta_bright = current_scheme.brights[6],
+        cyan_bright = current_scheme.brights[7],
+        white_bright = current_scheme.brights[8],
+    }
+
+    return key_to_color[key]
+end
 -- For example, changing the color scheme:
-config.color_scheme = 'AdventureTime'
+config.color_scheme = 'Apprentice (Gogh)'
+
+config.colors = {
+    tab_bar = {
+        active_tab = {
+            bg_color = 'olive',
+            -- The color of the text for the tab
+            fg_color = '#282803'
+        },
+        inactive_tab_hover = {
+            bg_color = '#3b3052',
+            fg_color = '#999990',
+            italic = false
+        }
+    }
+}
+
+config.font = wezterm.font 'FiraCode Nerd Font Mono'
+config.font_size = 18
+config.initial_rows = 30
+config.initial_cols = 50
+
+local padding = 24
+config.window_padding = {
+    left = padding,
+    right = padding,
+    top = padding,
+    bottom = padding,
+}
+
+config.inactive_pane_hsb = {
+    saturation = 0.8,
+    brightness = 0.7,
+}
+
+config.leader = { key = "Escape" }
+
+config.keys = {
+    {
+        mods = "LEADER",
+        key = "c",
+        action = wezterm.action.SpawnTab "CurrentPaneDomain",
+    },
+    {
+        mods = "LEADER",
+        key = "x",
+        action = wezterm.action.CloseCurrentPane { confirm = true }
+    },
+    {
+        mods = "LEADER",
+        key = "b",
+        action = wezterm.action.ActivateTabRelative(-1)
+    },
+    {
+        mods = "LEADER",
+        key = "n",
+        action = wezterm.action.ActivateTabRelative(1)
+    },
+    {
+        mods = "LEADER",
+        key = "d",
+        action = wezterm.action.SplitHorizontal { domain = "CurrentPaneDomain" }
+    },
+    {
+        mods = "LEADER",
+        key = "e",
+        action = wezterm.action.SplitVertical { domain = "CurrentPaneDomain" }
+    },
+    {
+        mods = "LEADER",
+        key = "h",
+        action = wezterm.action.ActivatePaneDirection "Left"
+    },
+    {
+        mods = "LEADER",
+        key = "j",
+        action = wezterm.action.ActivatePaneDirection "Down"
+    },
+    {
+        mods = "LEADER",
+        key = "k",
+        action = wezterm.action.ActivatePaneDirection "Up"
+    },
+    {
+        mods = "LEADER",
+        key = "l",
+        action = wezterm.action.ActivatePaneDirection "Right"
+    },
+    {
+        mods = "LEADER",
+        key = "LeftArrow",
+        action = wezterm.action.AdjustPaneSize { "Left", 5 }
+    },
+    {
+        mods = "LEADER",
+        key = "RightArrow",
+        action = wezterm.action.AdjustPaneSize { "Right", 5 }
+    },
+    {
+        mods = "LEADER",
+        key = "DownArrow",
+        action = wezterm.action.AdjustPaneSize { "Down", 5 }
+    },
+    {
+        mods = "LEADER",
+        key = "UpArrow",
+        action = wezterm.action.AdjustPaneSize { "Up", 5 }
+    },
+}
+
+-- activate tab 0 to 9
+for i = 1, 9 do
+    -- leader + number to activate that tab
+    table.insert(config.keys, {
+        key = tostring(i),
+        mods = "LEADER",
+        action = wezterm.action.ActivateTab(i - 1),
+    })
+end
+
+config.hide_tab_bar_if_only_one_tab = false
+config.tab_bar_at_bottom = true
+config.use_fancy_tab_bar = false
+config.tab_and_split_indices_are_zero_based = false
 
 -- and finally, return the configuration to wezterm
 return config
